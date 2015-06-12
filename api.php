875c5f4acf8910004c461726b2c6274dc6880df0
Alex Ayerdi <AlexanderAyerdi2015@u.northwestern.edu>
2015-06-12 13:54:21 -0500

    login for api.php

--- api.php
+++ api.php
@@ -1,18 +1,38 @@
 <?php
 
-$data = $_POST["data"];
-
-//blacklist all shell characters that could be used for shell injection
-$blacklist = ["|", "$", "&", ";", "<", ">", "`"];
-
-for ($i = 0; $i < count($blacklist); $i++)
+if (isset($_POST["data_query"]))
 {
-	$data = str_replace($blacklist[$i], "", $data);
+
+	$data = $_POST["data_query"];
+
+	$data = remove_malicious_characters($data);
+
+	echo shell_exec("ruby web-service.rb '$data'");
+}
+else if (isset($_POST["data_login"]))
+{
+	
+	$data = $_POST["data_login"];
+	
+	$data = remove_malicious_characters($data);
+	
+	echo shell_exec("ruby login.rb '$data'");
 }
 
-//escape all other characters that PHP deems could be used for shell injection
-$data = escapeshellcmd($data);
+function remove_malicious_characters($data)
+{
+	//blacklist all shell characters that could be used for shell injection
+	$blacklist = ["|", "$", "&", ";", "<", ">", "`"];
 
-echo shell_exec("ruby web-service.rb '$data'");
+	for ($i = 0; $i < count($blacklist); $i++)
+	{
+		$data = str_replace($blacklist[$i], "", $data);
+	}
+
+	//escape all other characters that PHP deems could be used for shell injection
+	$data = escapeshellcmd($data);
+	
+	return $data;
+}
 
 ?>
