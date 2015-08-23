8432490c25e10366351e9af67d143e6ad3e1b394
Alex Ayerdi <AlexanderAyerdi2015@u.northwestern.edu>
2015-08-23 16:36:18 -0500

    64 syntax fix, to string

--- Javascript/functions.js
+++ Javascript/functions.js
@@ -1081,7 +1081,7 @@ function OpenListing(id, address, unit, bedrooms, bathrooms, price, type, animal
     $("#modal-content-popup-listing .popup-ac").text("AC? " + BooleanToHumanReadable(AirConditioning));
     $("#modal-content-popup-listing .popup-tags").text("Tags: " + (!tags ? tags : tags.join(", ")));
     
-    $("#modal-content-popup-listing .owl-carousel-button").attr("onclick", "LoadOwlCarousel(" + id + ");");
+    $("#modal-content-popup-listing .owl-carousel-button").attr("onclick", "LoadOwlCarousel('" + id + "');");
     
     PopulateAndOpenModal(null, 'modal-content-popup-listing');
 }
