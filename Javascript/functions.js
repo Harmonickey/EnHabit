4f2cbd1f8f84dff47b48e5c7efd1ae3e0d634ad0
Alex Ayerdi <AlexanderAyerdi2015@u.northwestern.edu>
2015-11-15 01:59:18 -0600

    104 zoom control location

--- Javascript/functions.js
+++ Javascript/functions.js
@@ -24,8 +24,8 @@ var background_settings = {
 }
 
 L.mapbox.accessToken = 'pk.eyJ1IjoiaGFybW9uaWNrZXkiLCJhIjoiZmM4MGM0Mjk0NmJmMDFjMmY3YWY1NmUxMzllMzc5NGYifQ.hdx-TOA4rtQibXkpdLQK4g';
-var map = L.mapbox.map('map', 'mapbox.streets').setView([42.057, -87.680], 15);
-map.zoomControl = false;
+var map = L.mapbox.map('map', 'mapbox.streets', { zoomControl: false }).setView([42.057, -87.680], 15);
+new L.Control.Zoom({ position: 'topright' }).addTo(map);
 var markers = new L.FeatureGroup();
 //map.on('draw:created', getPointsWithinPolygon);
 
