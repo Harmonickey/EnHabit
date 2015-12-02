0ddc345a3fe70f074ef64ee7f8f7914dc3179a44
Alex Ayerdi <AlexanderAyerdi2015@u.northwestern.edu>
2015-12-02 00:21:56 -0600

    107 remove capital

--- tenant/listings/index.php
+++ tenant/listings/index.php
@@ -260,7 +260,7 @@
                 <div class="row">
                     <div class="col-lg-12 col-md-12 col-sm-12 ui-widget">
                         <label>University</label>
-                        <select id="Universities-filter" class="form-control">
+                        <select id="universities-filter" class="form-control">
                         
                         </select>
                     </div>
