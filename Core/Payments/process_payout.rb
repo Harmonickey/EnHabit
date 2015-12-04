95193b0d90c2a81efc28aa7f594639b4dbd8bccc
Alex Ayerdi <AlexanderAyerdi2015@u.northwestern.edu>
2015-12-03 23:26:39 -0600

    107 move to live

--- Core/Payments/process_payout.rb
+++ Core/Payments/process_payout.rb
@@ -19,9 +19,9 @@ ctx.ssl_version = :TLSv1
 
 @api = PayPal::SDK::REST.set_config(
     :ssl_options => { ssl_version: :TLSv1 }, # Set ssl options
-    :mode => :sandbox,  # Set :sandbox or :live
-    :client_id     => "AWLNvp25Gv70ELGi8ssjeB6XCiikBPGT_-Y0Sxjk2RFY7Elc4lWidqPGDsRLJcrOTFWeLkxVqY5sb1dV",
-    :client_secret => "EPwW3Clo9KQZgOeuWVgFY_q1jlKxpnAoFWMJDLxUUJ8nn3Z91XuQ6nN5AkBWy0JoGDxsLztc5AgdZoMI" )
+    :mode => :live,  # Set :sandbox or :live
+    :client_id     => "AejUVAu1yU3aCU9fRaNQk6D7HdKn-dQAUm5YZRxKW_TNENRwqAVan2EWGkhZXFTsSButt66LmJbNclc4",
+    :client_secret => "ECpvxnmfDmTK4lk39s6em0TpGxtVqVtC8o5C0E6YHxAbf6jbgrjIJNlUiriXDatSq6enxTOzNBLCmoeH" )
 
 @month = @data["Month"]
 @rent = @data["Rent"]
