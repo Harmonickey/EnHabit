64522a7733988db6681d851ea09939b3282c5f77
Alex Ayerdi <AlexanderAyerdi2015@u.northwestern.edu>
2015-12-07 01:38:33 -0600

    115 percent to money

--- admin/listings/index.php
+++ admin/listings/index.php
@@ -82,7 +82,7 @@
                     <li class="admin-nav"><a style="cursor: pointer;" href="/PaymentsPortal.php"><i class="fa fa-usd" style="margin-right: 5px;"></i>Payments</a></li>
                     <li class="admin-nav"><a style="cursor: pointer;" href="/RentersPortal.php"><i class="fa fa-users" style="margin-right: 5px;"></i>Renters</a></li>   
                     <li class="admin-nav"><a style="cursor: pointer;" href="/UniversitiesPortal.php"><i class="fa fa-bank" style="margin-right: 5px;"></i>Universities</a></li>
-                    <li class="admin-nav"><a style="cursor: pointer;" href="/PricingPortal.php"><i class="fa fa-percent" style="margin-right: 5px;"></i>Pricing</a></li>
+                    <li class="admin-nav"><a style="cursor: pointer;" href="/PricingPortal.php"><i class="fa fa-money" style="margin-right: 5px;"></i>Pricing</a></li>
                     <li id="login-function" class="menu-item scroll" onclick="Logout()">
                         <a id="login" style="cursor: pointer;"><i class="fa fa-sign-out" style="margin-right: 5px;"></i>Log Out</a>
                     </li>
@@ -140,7 +140,7 @@
 					</li>
                     <li>
 						<a href="../pricing">
-							<i class="fa fa-percent"></i>
+							<i class="fa fa-money"></i>
 							<span>Pricing</span>
 						</a>	    				
 					</li>
