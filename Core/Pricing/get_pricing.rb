34c5dec0c7be0652952cd9748f6d4b2c61196ef8
Alex Ayerdi <AlexanderAyerdi2015@u.northwestern.edu>
2015-12-06 20:55:04 -0600

    115 backend functions

---
+++ Core/Pricing/get_pricing.rb
@@ -0,0 +1,31 @@
+#!/usr/local/bin/ruby
+
+require 'json'
+require 'bson'
+require 'moped'
+
+Moped::BSON = BSON
+
+begin
+    mongoSession = Moped::Session.new(['127.0.0.1:27017'])# our mongo database is local
+    mongoSession.use("enhabit")# this is our current database
+
+    documents = Array.new
+    mongoSession.with(safe: true) do |session|
+        documents = session[:pricing].find().select(_id: 1, UniversityId: 1, ListingMarkup: 1, FeaturedMarkup: 1).to_a
+    end
+    mongoSession.disconnect
+
+    if documents.count == 0
+        puts "No Pricing Markups"
+    else
+        puts documents.to_json
+    end
+rescue Exception => e
+    File.open("error.log", "a") do |output|
+        output.puts e.message
+        output.puts e.backtrace.inspect
+    end
+
+    puts "Error: #{e.message}"
+end
---
+++ Core/Pricing/update_pricing.rb
@@ -0,0 +1,63 @@
+#!/usr/local/bin/ruby
+
+absPath = Dir.pwd
+base = absPath.split("/").index("public_html")
+deploymentBase = absPath.split("/")[0..(base + 1)].join("/") #this will reference whatever deployment we're in
+
+$: << "#{deploymentBase}/Libraries"
+
+require 'json'
+require 'moped'
+require 'bson'
+require 'PasswordHash'
+require 'tools'
+
+Moped::BSON = BSON
+
+def UpdatePricing(id, listingMarkup, featuredMarkup)
+
+    #id is ObjectId in :universities
+    mongoSession = Moped::Session.new(['127.0.0.1:27017']) # our mongo database is local
+    mongoSession.use("enhabit") # this is our current database
+
+    universityObj = Hash.new
+    universityObj["_id"] = Moped::BSON::ObjectId.from_string(id.to_s)
+    
+    retMsg = ""
+ 
+    begin
+        university = Hash.new
+    
+        mongoSession.with(safe: true) do |session|
+            university = session[:universities].find(universityObj).one
+        end
+        
+        universityId = university["UniversityId"]
+    
+        queryObj = Hash.new
+        queryObj["UniversityId"] = universityId
+    
+        pricingObj = Hash.new
+        pricingObj["ListingMarkup"] = listingMarkup
+        pricingObj["FeaturedMarkup"] = featuredMarkup
+    
+        mongoSession.with(safe: true) do |session|
+            session[:pricing].find(queryObj).update('$set' => pricingObj)
+        end
+    
+        retMsg = "Okay"
+    rescue Moped::Errors::OperationFailure => e
+        retMsg = e
+    end
+    
+	mongoSession.disconnect
+    return retMsg
+end
+
+begin
+    data = JSON.parse(ARGV[0].delete('\\')) unless ARGV[0].empty?
+    
+    puts UpdatePricing(data["id"], data["ListingMarkup"], data["FeaturedMarkup"])
+rescue Exception => e
+    puts e.inspect
+end
