e1e8f31f78a968454827deff81a3a1a939f69d2d
Alex Ayerdi <AlexanderAyerdi2015@u.northwestern.edu>
2015-12-08 00:01:11 -0600

    117 renters 2

--- Javascript/tenant/functions.js
+++ Javascript/tenant/functions.js
@@ -1025,7 +1025,7 @@ function GetPayKey(oid, data)
                 
                 if (payResponse["error"])
                 {
-                   throw Error("Unable to Process Payment"); 
+                   throw Error("Unable to Get Payment Key"); 
                 }
                 else
                 {
