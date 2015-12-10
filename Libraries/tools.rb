bf764e0e87a627cd3f5082afdc650ae8f9bb261c
Alex Ayerdi <AlexanderAyerdi2015@u.northwestern.edu>
2015-12-10 00:34:40 -0600

    119 seriously... spelled wrong

--- Libraries/tools.rb
+++ Libraries/tools.rb
@@ -16,7 +16,7 @@ def ComputeDistance(lat1, lon1, lat2, lon2)
     phi1 = lat1.to_f.to_rad
     phi2 = lat2.to_f.to_rad
     deltaphi = (lat2.to_f - lat1.to_f).to_rad
-    deltalamba = (lon2.to_f - lon1.to_f).to_rad
+    deltalambda = (lon2.to_f - lon1.to_f).to_rad
 
     a = Math.sin(deltaphi/2) * Math.sin(deltaphi/2) + Math.cos(phi1) * Math.cos(phi2) * Math.sin(deltalambda/2) * Math.sin(deltalambda/2);
     c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a))
