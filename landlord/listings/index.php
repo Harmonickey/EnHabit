c667221a26f720fd7f950184776a1c7b77e3d037
Alex Ayerdi <AlexanderAyerdi2015@u.northwestern.edu>
2016-01-04 21:33:02 -0600

    121 change logout methods, update payment

--- landlord/listings/index.php
+++ landlord/listings/index.php
@@ -4,7 +4,7 @@
     
     if (!isset($_SESSION["landlordId"]))
     {
-        header("Location: /#loggedout", FALSE);
+        header("Location: /#sessiontimeout", FALSE);
         return;
     }
     
