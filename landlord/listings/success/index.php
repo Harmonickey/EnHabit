1893860f474709bd101ad46d94f555999806c9a2
Alex Ayerdi <AlexanderAyerdi2015@u.northwestern.edu>
2016-01-21 22:46:32 -0600

    116 featured listings

---
+++ landlord/listings/success/index.php
@@ -0,0 +1,21 @@
+<?php
+ 
+    $landlordEmail = $_GET["landlordEmail"];
+    $amount = $_GET["amount"];
+    $oid = $_GET["oid"];
+
+?>
+
+<!DOCTYPE html>
+<html lang="en"> 
+    <head>
+        <meta charset="utf-8">
+        <script src="../../../Libraries/Javascript/jquery-1.9.1.min.js"></script>
+        <script src="../../../Javascript/paymentsandbox/functions.js"></script>
+    </head>
+    <script type="text/javascript">
+    
+        <?php echo "InsertFeaturedPayment('" . $oid . "', '" . $landlordEmail . "', '" . $amount . "')"; ?>
+
+    </script>
+</html>
