4b03363cfb62ee1f84b201daff7f94c7a7d809a5
Alex Ayerdi <AlexanderAyerdi2015@u.northwestern.edu>
2016-02-21 19:29:21 -0600

    140 remove console logs

--- Javascript/functions.js
+++ Javascript/functions.js
@@ -72,7 +72,6 @@ function SubscribeSlideshowArrows()
 {
     $('body').on('click', '.slider-arrow img', function() 
     {
-        console.log("here");
         var $slideshow = $(this).closest('.slideshow');
         var $newSlide;
         
@@ -1984,8 +1983,6 @@ function CloseExtrasView()
 
 function CreateListing()
 {   
-    console.log("here");
-
     var data = listingData;
     
     try
