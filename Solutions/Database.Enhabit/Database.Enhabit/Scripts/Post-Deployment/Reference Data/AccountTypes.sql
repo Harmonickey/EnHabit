fc2282250aec387741ed2ed5ed918acce8ba2fac
Alex Ayerdi <AlexanderAyerdi2015@u.northwestern.edu>
2016-03-06 16:06:51 -0600

    143 sql agent user

---
+++ Solutions/Database.Enhabit/Database.Enhabit/Scripts/Post-Deployment/Reference Data/AccountTypes.sql
@@ -0,0 +1,22 @@
+﻿/*
+Post-Deployment Script Template							
+--------------------------------------------------------------------------------------
+ This file contains SQL statements that will be appended to the build script.		
+ Use SQLCMD syntax to include a file in the post-deployment script.			
+ Example:      :r .\myfile.sql								
+ Use SQLCMD syntax to reference a variable in the post-deployment script.		
+ Example:      :setvar TableName MyTable							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+
+MERGE INTO [Enhabit].[AccountTypes] AS TARGET
+USING (VALUES 
+	(0, 'Admin'),
+	(1, 'Landlord'),
+	(2, 'Tenant'))
+AS SOURCE([AccountTypeId], [AccountType])
+ON SOURCE.[AccountTypeId] = TARGET.[AccountTypeId]
+WHEN NOT MATCHED BY TARGET THEN
+	INSERT ([AccountTypeId], [AccountType])
+	VALUES ([BuildingTypeId], [BuildingType]);
