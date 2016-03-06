1ed0a671b5661a8a20b3580ecb4731529d9e8596
Alex Ayerdi <AlexanderAyerdi2015@u.northwestern.edu>
2016-03-06 15:54:19 -0600

    143 publish success, added post script

---
+++ Solutions/Database.Enhabit/Database.Enhabit/Scripts/Post-Deployment/Reference Data/ParkingOptions.sql
@@ -0,0 +1,24 @@
+﻿/*
+Post-Deployment Script Template							
+--------------------------------------------------------------------------------------
+ This file contains SQL statements that will be appended to the build script.		
+ Use SQLCMD syntax to include a file in the post-deployment script.			
+ Example:      :r .\myfile.sql								
+ Use SQLCMD syntax to reference a variable in the post-deployment script.		
+ Example:      :setvar TableName MyTable							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+
+MERGE INTO [Enhabit].[ParkingOptions] AS TARGET
+USING (VALUES 
+	(0, 'None'),
+	(1, 'Garage'),
+	(2, 'OutsidePrivateLot'),
+	(3, 'Street'))
+AS SOURCE([ParkingId], [ParkingType])
+ON SOURCE.[ParkingId] = TARGET.[ParkingId]
+WHEN NOT MATCHED BY TARGET THEN
+	INSERT ([ParkingId], [ParkingType])
+	VALUES ([ParkingId], [ParkingType]);
+
---
+++ Solutions/Database.Enhabit/Database.Enhabit/Scripts/Post-Deployment/Reference Data/PetOptions.sql
@@ -0,0 +1,23 @@
+﻿/*
+Post-Deployment Script Template							
+--------------------------------------------------------------------------------------
+ This file contains SQL statements that will be appended to the build script.		
+ Use SQLCMD syntax to include a file in the post-deployment script.			
+ Example:      :r .\myfile.sql								
+ Use SQLCMD syntax to reference a variable in the post-deployment script.		
+ Example:      :setvar TableName MyTable							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+
+MERGE INTO [Enhabit].[PetOptions] AS TARGET
+USING (VALUES 
+	(0, 'None'),
+	(1, 'Cats'),
+	(2, 'Dogs'))
+AS SOURCE([PetId], [PetType])
+ON SOURCE.[PetId] = TARGET.[PetId]
+WHEN NOT MATCHED BY TARGET THEN
+	INSERT ([PetId], [PetType])
+	VALUES ([PetId], [PetType]);
+
